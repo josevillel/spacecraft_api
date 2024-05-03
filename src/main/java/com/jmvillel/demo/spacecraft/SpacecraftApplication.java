3e3eea0b36cf54892d5505ed92a89847aaf31695
Jose Villel <jmvillel@aubay.es>
2024-05-04 00:34:43 +0200

    Estructura y deploy inicial

---
+++ src/main/java/com/jmvillel/demo/spacecraft/SpacecraftApplication.java
@@ -0,0 +1,13 @@
+package com.jmvillel.demo.spacecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacecraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacecraftApplication.class, args);
+	}
+
+}
